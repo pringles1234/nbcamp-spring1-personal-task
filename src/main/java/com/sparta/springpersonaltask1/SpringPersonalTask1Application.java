e82fe844192a0b949b327e5fd4820d5a5756ed9a
Yujin Lee <dbwls9040@gmail.com>
2024-08-13 16:12:15 +0900

    init commit

---
+++ src/main/java/com/sparta/springpersonaltask1/SpringPersonalTask1Application.java
@@ -0,0 +1,13 @@
+package com.sparta.springpersonaltask1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPersonalTask1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPersonalTask1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/springpersonaltask1/SpringPersonalTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springpersonaltask1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPersonalTask1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
